619db5ab5df38def36548fc5711be8b8669b51f9
Wyatt Mercer <mercerwyatt390@gmail.com>
2026-01-17 19:29:02 -0600

    -added files logger, constants, and added game_state.jsonl to .gitignore file
    -updated main.py to print Screen width & height
    
    -now when running main.py a window with a black screen pops up and I can close it with Ctr+C in terminal or hit the top right "X" to close it properly.

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,8 +1,23 @@
 import pygame
+import constants
+from logger import log_state
+
+
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-
+    SCREEN_WIDTH = constants.SCREEN_WIDTH 
+    SCREEN_HEIGHT = constants.SCREEN_HEIGHT
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}", 
+          f"\nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
