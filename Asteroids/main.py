075467a218cd96a786e40bf0594a3025af39e92b
Wyatt Mercer <mercerwyatt390@gmail.com>
2026-01-18 21:23:26 -0600

    -Added new files (player.py, circleshape.py)
    -added new variables in constant.py
    
    -updated main.py to add a player and better refresh mechanics to lesson the cpu intensity
    
    -player.py has all player movements within it and uses variables from constaint.py and delta time from main.py

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,23 +1,60 @@
 import pygame
 import constants
 from logger import log_state
+import player
 
 
 
 def main():
-    SCREEN_WIDTH = constants.SCREEN_WIDTH 
-    SCREEN_HEIGHT = constants.SCREEN_HEIGHT
+
+    # Variables
+    SCREEN_WIDTH = constants.SCREEN_WIDTH   # Screen
+    SCREEN_HEIGHT = constants.SCREEN_HEIGHT # Resolution
+    dt = 0  # Delta Time
+
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}", 
           f"\nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+    
+    # Initializing pygame function
     pygame.init()
+
+    # Delta time clock
+    clock = pygame.time.Clock()
+    
+
+    # Screen resolution
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Groups 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = updatable, drawable
+    
+    # Player 
+    PLAYER = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    # Game loop
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        
+        # Calculate delta time FIRST
+        dt = clock.tick(60) / 1000  # FPS clock
+        
+        # Update game state
+        updatable.update(dt)
+        
+        # Clear and draw
+        screen.fill("black")  # Background color
+        for sprite in drawable:
+            sprite.draw(screen)  # Draw each sprite using its custom draw method
+        
+        # Screen refresh
         pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
